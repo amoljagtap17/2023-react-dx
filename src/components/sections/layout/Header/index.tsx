1b3f14035a9911137fd4bd4f52a836d5908bd196
amoljagtap17 <amoljagtap17@gmail.com>
2023-10-14 18:57:21 +0530

    react-dx: interim commit

--- src/components/sections/layout/Header/index.tsx
+++ src/components/sections/layout/Header/index.tsx
@@ -1,6 +1,7 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import {
   AppBar,
+  Box,
   Container,
   IconButton,
   Link,
@@ -12,30 +13,35 @@ import { LoginButton } from "./LoginButton";
 
 export function Header() {
   return (
-    <AppBar position="static" square elevation={0}>
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Link
-            color="inherit"
-            href="/"
-            underline="none"
-            component={NextLink}
-            sx={{ flexGrow: 1 }}
-          >
-            <Typography variant="h6">React DX</Typography>
-          </Link>
-          <LoginButton />
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Box>
+      <AppBar component="nav" position="fixed" square elevation={0}>
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Link
+              color="inherit"
+              href="/"
+              underline="none"
+              component={NextLink}
+              sx={{ flexGrow: 1 }}
+            >
+              <Typography variant="h6">React DX</Typography>
+            </Link>
+            <LoginButton />
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box padding={2}>
+        <Toolbar />
+      </Box>
+    </Box>
   );
 }
